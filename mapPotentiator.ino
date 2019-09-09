7820855f5a1ade34a97c8ae2e979a3b515720046
James Knight <james.p.knight2@gmail.com>
2019-09-09 16:29:21 -0600

    Update mapPotentiator.ino

--- mapPotentiator.ino
+++ mapPotentiator.ino
@@ -1,13 +1,21 @@
+  int const ledPin = 5;
+  int const buttonA = 4;
+  int const potPin = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonA, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-  int potValue {150};
+  
+  int potValue {0};
   int const fromLow {0};
   int const fromHigh {1023};
   int const toLow {0};
   int const toHigh{255};
+
+  
   int myMap(int val, int fromLow, int fromHigh, int toLow, int toHigh)
   {
     int toRange = (toHigh - toLow);
@@ -16,8 +24,19 @@ void setup() {
     int result = (val / ratio);
     return constrain(result, toLow, toHigh);
   }
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(map(potValue, fromLow, fromHigh, toLow, toHigh));
+  
+void loop() { 
+  potValue = analogRead(potPin);
+  int const mapValue = map(potValue, fromLow, fromHigh, toLow, toHigh);
+  if(analogRead(buttonA) == LOW) {
+    analogWrite(ledPin, mapValue);
+    delay(500);
+    analogWrite(ledPin, 0);
+    delay(500);
+  }
+
+  
+
+  Serial.println(mapValue);
   Serial.println(myMap(potValue, fromLow, fromHigh, toLow, toHigh));
 }
